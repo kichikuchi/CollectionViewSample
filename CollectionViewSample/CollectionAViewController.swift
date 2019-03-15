791a1b7feb32ad8383d183a5fd665b130e66502c
kichikuchi <kikuchi0111@gmail.com>
2019-03-16 01:42:11 +0900

    first commit

---
+++ CollectionViewSample/CollectionAViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CollectionAViewController.swift
+//  CollectionViewSample
+//
+//  Created by Kazunori Kikuchi on 2019/03/15.
+//  Copyright © 2019 kazunori kikuchi. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+private let headerIdentifier = "Header"
+
+class CollectionAViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 2 - 1, height: UIScreen.main.bounds.width / 2 - 1)
+        layout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 100)
+        layout.minimumInteritemSpacing = 1
+        collectionView.collectionViewLayout = layout
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath)
+        view.backgroundColor = .yellow
+        return view
+    }
+}
---
+++ CollectionViewSample/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by Kazunori Kikuchi on 2019/03/15.
+//  Copyright © 2019 kazunori kikuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {}
